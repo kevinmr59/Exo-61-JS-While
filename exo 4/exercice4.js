7938e7aae75c9ac3d74d30d7f14531701893cd3f
kevinmr59 <kevinmura59610@gmail.com>
2021-11-15 17:54:04 +0100

    while

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,9 +1,9 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
 var i = 15;
-while (i   ) {
+while (i <2 ) {
     document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i;
-    =   ;
+     i++ ;
 }
 
 
